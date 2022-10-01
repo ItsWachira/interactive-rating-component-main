a893fae1005e38fed38e122569e664ff08a985b5
ItsWachira <brianwachira79@gmail.com>
2022-10-01 16:18:09 +0300

    second commit

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
-function GetRating() {
-    var ratingValue = document.getElementById("rating").value;
-    // return rating;
-    document.getElementById("rating-selected").innerHTML = parseInt(ratingValue);
-}
 
+document.querySelector(".main2").style.display="none";
+
+document.querySelector(".btn").addEventListener("click", function() {
+    document.querySelector(".main").style.display="none";
+    document.querySelector(".main2").style.display="";
+});
+
+for(var i= 0; i<5; i++){
+    document.querySelectorAll(".item")[i].addEventListener("click", function() {
+        this.style.backgroundColor="hsl(217, 12%, 63%)";
+        this.style.color="white";
+        var rate = this.innerHTML;
+        var str = "You selected "+rate+" out of 5";
+        document.querySelector(".select").innerHTML=str;
+
+    });
+}
